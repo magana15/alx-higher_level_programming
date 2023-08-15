4e1d6dd02737b288e33c41e98afec79a31411202
joel  magana <joelmagana15@gmail.com>
2023-08-15 04:46:03 -0700

    correct

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    def car():#carrier
+        total = 0
+        for i in range(1,len(sys.argv)):
+            total += int(sys.argv[i])
+        print(total)
+        car()
 
