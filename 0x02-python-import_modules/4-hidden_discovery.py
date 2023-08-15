09ee03ceb6bef639a4ed776d8f5607d876dbb624
joel  magana <joelmagana15@gmail.com>
2023-08-15 05:36:26 -0700

    corr

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@
 
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+ 
+    for name in dir(hidden_4):
+        if not(i[0] == '_' and i[1] == '_'):
             print(name)
 if __name__ == "__main__":
