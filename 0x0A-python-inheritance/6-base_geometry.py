7fc054a4fe1c78e483a47549ddac0333deb2a6b4
joel magana <joelmagana15@gmail.com>
2024-02-05 06:32:25 -0800

    the project

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry based on 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry.
+    """
+    def area(self):
+        """Area function.
+
+        Raise:
+            Exception: if area is not implemented.
+        """
+        raise Exception("area() is not implemented")
